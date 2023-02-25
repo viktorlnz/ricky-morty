6b9974a7e7da0dbd943a05670baae334e2efaedf
viktorlnz <lnz.viktor0@gmail.com>
2023-02-25 18:54:20 -0300

    Inicio

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> Ricky and Morty </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer>
+      <div class="text-body2">&copy; Todos os direitos reservados</div>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup lang="ts"></script>
